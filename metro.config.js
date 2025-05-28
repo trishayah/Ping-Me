23d941a34b1d854c9337dd13aca7dce89164b6f3
trishiah-rellon <trishiah.rellon@ctu.edu.ph>
2025-05-28 22:37:18 +0800

    updated metro config

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-defaultConfig.resolver.sourceExts.push('cjs');
 
-module.exports = defaultConfig;
+// Add additional module resolution
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs', 'mjs'];
+config.resolver.assetExts = [...config.resolver.assetExts, 'db'];
+
+// Enable Node.js modules
+config.resolver.extraNodeModules = {
+    ...config.resolver.extraNodeModules,
+    tslib: require.resolve('tslib'),
+};
+
+module.exports = config;
